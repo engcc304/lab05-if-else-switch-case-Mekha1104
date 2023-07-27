65fdb6951f08cdb27468d4f47917a74d1dc3caf6
Mekha1104 <gtrcooie@gmail.com>
2023-07-27 15:06:58 +0700

    Lab5

--- Lab/Lab05-If-Else-Switch-Case.cpp
+++ Lab/Lab05-If-Else-Switch-Case.cpp
@@ -44,7 +44,7 @@ int main() {
     printf( "Grade : " ) ;
     if( score >= 80 ) {
         printf( " A " ) ;
-    } else if( score >= 75 && score < 80 ) {
+    } else if( score >= 75 && score < 80 ){
         printf( " B+ " ) ;
     } else if( score >= 70 && score < 75 ){
         printf( " B " ) ;
