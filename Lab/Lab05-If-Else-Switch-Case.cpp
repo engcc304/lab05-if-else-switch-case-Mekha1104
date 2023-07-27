4bc404b126cfc639a9826e8458eca12812ca8bf2
Mekha1104 <gtrcooie@gmail.com>
2023-07-27 14:45:39 +0700

    Lab5

--- Lab/Lab05-If-Else-Switch-Case.cpp
+++ Lab/Lab05-If-Else-Switch-Case.cpp
@@ -38,6 +38,28 @@
 int main() {
 
     //--| YOUR CODE HERE
-
+    int score = 0 ;
+    printf( "Please enter your score : " ) ;
+    scanf( "%d" , &score ) ;
+    printf( "Grade : " ) ;
+    if( score >= 80 ) {
+        printf( " A !" ) ;
+    } else if( score >= 75 && score < 80 ) {
+        printf( " B+ !" ) ;
+    } else if( score >= 70 && score < 75 ){
+        printf( " B !" ) ;
+    } else if( score >= 65 && score < 70 ){
+        printf( " C+ !") ;
+    } else if( score >= 60 && score < 65 ){
+        printf( " C !" ) ;
+    } else if( score >= 55 && score < 60 ){
+        printf( " D + !") ;
+    } else if( score >= 50 && score < 55){
+        printf( " D ! ") ;
+    } else if( !score ){
+        printf("please enter number only.") ;
+    } else {
+         printf(" F ! ") ;
+    }//end if
     return 0 ;
 }//end main function
